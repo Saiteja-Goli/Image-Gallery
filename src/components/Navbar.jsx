3a527618f3b8b251aad669fd170e9e69ea85aaf9
Saiteja-Goli <saitejagoli111@gmail.com>
2024-02-25 08:41:37 +0530

    Responsiveness Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import "../styles/Navbar.css" 
+import "../styles/Navbar.css"
 import { faHome, faImages, faEnvelope, faInfoCircle, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
     const toggleNav = () => {
-      setIsNavOpen(!isNavOpen);
+        setIsNavOpen(!isNavOpen);
     };
 
     return (
         <nav className="navbar">
             <div className="navbar-left">
                 <ul className={`navbar-links ${isNavOpen ? 'active' : ''}`}>
-                    <li><FontAwesomeIcon icon={faHome} /> Home</li>
-                    <li><FontAwesomeIcon icon={faImages} /> Gallery</li>
-                    <li><FontAwesomeIcon icon={faEnvelope} /> Contact</li>
-                    <li><FontAwesomeIcon icon={faInfoCircle} /> About</li>
+                    <div className='nav-links'>
+                        <li ><FontAwesomeIcon icon={faHome} /> Home</li>
+                    </div>
+                    <div className='nav-links'>
+                        <li><FontAwesomeIcon icon={faImages} /> Gallery</li>
+                    </div>
+                    <div className='nav-links'>
+                        <li><FontAwesomeIcon icon={faEnvelope} /> Contact</li>
+                    </div>
+                    <div className='nav-links'>
+                        <li><FontAwesomeIcon icon={faInfoCircle} /> About</li>
+                    </div>
                 </ul>
             </div>
             <div className="navbar-toggle" onClick={toggleNav}>
